a195b4a544e687ca009e24830067256ba1cefd93
Hakira-Shymuy <72995374+Hakira-Shymuy@users.noreply.github.com>
2023-04-22 19:18:14 +0200

    Main Commit FStudio alpha 0.0.2

---
+++ plugin.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Plugin Name: FStudio Public for breakdance
+ * Plugin URI: https://github.com/Hakira-Shymuy/Fstudio-public
+ * Description: Alpha version
+ * Author: Nuno Ferreira
+ * Author URI: https://github.com/Hakira-Shymuy
+ * License: GPLv3
+ * Text Domain: breakdance
+ * Domain Path: /languages/
+ * Version: 0.0.2 - Alpha
+ */
+
+namespace FStudioCElements;
+
+use function Breakdance\Util\getDirectoryPathRelativeToPluginFolder;
+
+add_action('breakdance_loaded', function () {
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/elements',
+        'FStudioCElements',
+        'element',
+        'Custom Elements',
+        false
+    );
+
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/macros',
+        'FStudioCElements',
+        'macro',
+        'Custom Macros',
+        false,
+    );
+
+    \Breakdance\ElementStudio\registerSaveLocation(
+        getDirectoryPathRelativeToPluginFolder(__DIR__) . '/presets',
+        'FStudioCElements',
+        'preset',
+        'Custom Presets',
+        false,
+    );
+},
+    // register elements before loading them
+    9
+);
