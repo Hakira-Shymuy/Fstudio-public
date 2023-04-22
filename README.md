10b092b776af4b02752754c1c4322e11f1262f16
Hakira-Shymuy <72995374+Hakira-Shymuy@users.noreply.github.com>
2023-04-22 19:18:22 +0200

    Main Commit FStudio alpha 0.0.2

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Public Version of FStudio elements for breakdance
 
 ## What is FStudio
 
-FStudio is my own web company and this elements are used by my self and by my clients in Breakdance Page Builder
+FStudio is my own web company and this elements are used by my self and by my clients in Breakdance Page Builder ( I have my own private version )
 
 ### Why i build it?
 
@@ -20,3 +20,23 @@ However, i will not be responsible for any problem, damage, incompatibility or a
 ### Do i provide support?
 
 I do not provide any official Support, however, i can try to help
+
+### Where can i reach you?
+
+I will later on provide bugs and support on Github, when i set up the things later on
+
+For now:
+You can contact me or find me at
+
+#### What is diferent from your elements vs breakdance elements
+
+First of all, i do not want to provide anything that breakdance already provides ( besides ) the way that things work
+They are adapted for my own usability, the way that i think that makes more sense, IF that does not match how you think or, if it does not make sense, then do not install the plugin
+
+Container is the main element here, is nothing more than a Div with all the Breakdance Div capabilities, but the controls have a diferent layout
+Also, it have grid controls ( created native with breakdance elements studio )
+
+#### Do i need to wait for Breakdance Grid or will this be better?
+
+I do not know and i do not want to replace Breakdance Grid in the future
+I want features that i need ( Right now ) and on ( My way )
---
+++ elements/Container/element.php
@@ -0,0 +1,284 @@
+<?php
+
+namespace FStudioCElements;
+
+use function Breakdance\Elements\c;
+use function Breakdance\Elements\PresetSections\getPresetSection;
+
+
+\Breakdance\ElementStudio\registerElementForEditing(
+    "FStudioCElements\\Container",
+    \Breakdance\Util\getdirectoryPathRelativeToPluginFolder(__DIR__)
+);
+
+class Container extends \Breakdance\Elements\Element
+{
+    static function uiIcon()
+    {
+        return 'SquareIcon';
+    }
+
+    static function tag()
+    {
+        return 'div';
+    }
+
+    static function tagOptions()
+    {
+        return ['section', 'header', 'footer', 'nav', 'aside', 'figure', 'ul', 'ol', 'li', 'article', 'details', 'summary', 'p', 'span'];
+    }
+
+    static function tagControlPath()
+    {
+        return false;
+    }
+
+    static function name()
+    {
+        return 'Container';
+    }
+
+    static function className()
+    {
+        return 'fscontainer';
+    }
+
+    static function category()
+    {
+        return 'basic';
+    }
+
+    static function badge()
+    {
+        return false;
+    }
+
+    static function slug()
+    {
+        return get_class();
+    }
+
+    static function template()
+    {
+        return file_get_contents(__DIR__ . '/html.twig');
+    }
+
+    static function defaultCss()
+    {
+        return file_get_contents(__DIR__ . '/default.css');
+    }
+
+    static function defaultProperties()
+    {
+        return ['design' => ['grid_layout' => ['is_child' => null, 'column_span' => null], 'size' => ['width' => ['breakpoint_base' => ['number' => 100, 'unit' => '%', 'style' => '100%']]]]];
+    }
+
+    static function defaultChildren()
+    {
+        return false;
+    }
+
+    static function cssTemplate()
+    {
+        $template = file_get_contents(__DIR__ . '/css.twig');
+        return $template;
+    }
+
+    static function designControls()
+    {
+        return [getPresetSection(
+      "EssentialElements\\layout",
+      "Flex Layout",
+      "flex_layout",
+       ['type' => 'popout']
+     ), c(
+        "grid_layout",
+        "Grid Layout",
+        [c(
+        "is_grid",
+        "Is Grid",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "auto_columns",
+        "Auto Columns",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline', 'triggerActionsButtonOptions' => ['text' => 'Auto Columns'], 'condition' => ['0' => ['0' => ['path' => 'design.grid_layout.is_child', 'operand' => 'is not set', 'value' => ''], '1' => ['path' => 'design.grid_layout.is_grid', 'operand' => 'is set', 'value' => '']]]],
+        true,
+        false,
+        [],
+      ), c(
+        "auto_min",
+        "Auto Min",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'unitOptions' => ['types' => ['0' => 'px', '1' => 'rem', '2' => '%', '3' => 'vw', '4' => 'custom', '5' => 'calc']], 'condition' => ['0' => ['0' => ['path' => 'design.grid_layout.auto_columns', 'operand' => 'is set', 'value' => '']]]],
+        true,
+        false,
+        [],
+      ), c(
+        "auto_max",
+        "Auto Max",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'unitOptions' => ['types' => ['0' => 'px', '1' => 'rem', '2' => '%', '3' => 'vw', '4' => 'custom', '5' => 'calc']], 'condition' => ['0' => ['0' => ['path' => 'design.grid_layout.auto_columns', 'operand' => 'is set', 'value' => '']]]],
+        true,
+        false,
+        [],
+      ), c(
+        "grid_columns",
+        "Grid Columns",
+        [],
+        ['type' => 'number', 'layout' => 'inline', 'rangeOptions' => ['min' => 1, 'max' => 12, 'step' => 1], 'condition' => ['0' => ['0' => ['path' => 'design.grid_layout.auto_columns', 'operand' => 'is not set', 'value' => ''], '1' => ['path' => 'design.grid_layout.is_child', 'operand' => 'is not set', 'value' => ''], '2' => ['path' => 'design.grid_layout.is_grid', 'operand' => 'is set', 'value' => '']]]],
+        true,
+        false,
+        [],
+      ), c(
+        "column_gap",
+        "Column Gap",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'unitOptions' => ['types' => ['0' => 'px', '1' => 'em', '2' => 'rem', '3' => '%', '4' => 'vw', '5' => 'custom'], 'defaultType' => 'px'], 'condition' => ['0' => ['0' => ['path' => 'design.grid_layout.is_child', 'operand' => 'is not set', 'value' => ''], '1' => ['path' => 'design.grid_layout.is_grid', 'operand' => 'is set', 'value' => '']]]],
+        true,
+        false,
+        [],
+      ), c(
+        "row_gap",
+        "Row Gap",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'unitOptions' => ['types' => ['0' => 'px', '1' => 'em', '2' => 'rem', '3' => '%', '4' => 'vw', '5' => 'calc'], 'defaultType' => 'px'], 'condition' => ['0' => ['0' => ['path' => 'design.grid_layout.is_child', 'operand' => 'is not set', 'value' => ''], '1' => ['path' => 'design.grid_layout.is_grid', 'operand' => 'is set', 'value' => '']]]],
+        true,
+        false,
+        [],
+      ), c(
+        "grid_alert",
+        "Grid Alert",
+        [],
+        ['type' => 'alert_box', 'layout' => 'vertical', 'alertBoxOptions' => ['style' => 'warning', 'content' => '<p>CSS Grid <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout">https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Grid_Layout</a></p>']],
+        false,
+        false,
+        [],
+      ), c(
+        "is_child",
+        "Is Child",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline', 'condition' => ['0' => ['0' => ['path' => 'design.grid_layout.is_grid', 'operand' => 'is not set', 'value' => '']]]],
+        false,
+        false,
+        [],
+      ), c(
+        "column_span",
+        "Column Span",
+        [],
+        ['type' => 'number', 'layout' => 'inline', 'rangeOptions' => ['min' => 1, 'max' => 12, 'step' => 1], 'condition' => ['0' => ['0' => ['path' => 'design.grid_layout.is_child', 'operand' => 'is set', 'value' => '']]]],
+        true,
+        false,
+        [],
+      )],
+        ['type' => 'section'],
+        false,
+        false,
+        [],
+      ), getPresetSection(
+      "EssentialElements\\size",
+      "Size",
+      "size",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\spacing_all",
+      "Spacing",
+      "spacing",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\borders",
+      "Borders",
+      "borders",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\fancy_background",
+      "Background",
+      "background",
+       ['type' => 'popout']
+     )];
+    }
+
+    static function contentControls()
+    {
+        return [];
+    }
+
+    static function settingsControls()
+    {
+        return [];
+    }
+
+    static function dependencies()
+    {
+        return false;
+    }
+
+    static function settings()
+    {
+        return false;
+    }
+
+    static function addPanelRules()
+    {
+        return false;
+    }
+
+    static public function actions()
+    {
+        return false;
+    }
+
+    static function nestingRule()
+    {
+        return ["type" => "container",   ];
+    }
+
+    static function spacingBars()
+    {
+        return false;
+    }
+
+    static function attributes()
+    {
+        return false;
+    }
+
+    static function experimental()
+    {
+        return false;
+    }
+
+    static function order()
+    {
+        return 20;
+    }
+
+    static function dynamicPropertyPaths()
+    {
+        return ['0' => ['accepts' => 'image_url', 'path' => 'design.background.image'], '1' => ['accepts' => 'image_url', 'path' => 'design.background.overlay.image'], '2' => ['accepts' => 'image_url', 'path' => 'design.background.layers[].image']];
+    }
+
+    static function additionalClasses()
+    {
+        return false;
+    }
+
+    static function projectManagement()
+    {
+        return false;
+    }
+
+    static function propertyPathsToWhitelistInFlatProps()
+    {
+        return ['design.background.image', 'design.background.overlay.image', 'design.background.image_settings.unset_image_at', 'design.background.image_settings.size', 'design.background.image_settings.height', 'design.background.image_settings.repeat', 'design.background.image_settings.position', 'design.background.image_settings.left', 'design.background.image_settings.top', 'design.background.image_settings.attachment', 'design.background.image_settings.custom_position', 'design.background.image_settings.width', 'design.background.overlay.image_settings.custom_position', 'design.background.image_size', 'design.background.overlay.image_size', 'design.background.overlay.type', 'design.background.design.layout.horizontal.vertical_at', 'design.background.image_settings', 'design.grid_child.spans', 'design.grid_layout.column_span', 'design.grid_layout.is_child', 'design.grid_layout.grid_columns', 'design.grid_layout.is_grid'];
+    }
+
+    static function propertyPathsToSsrElementWhenValueChanges()
+    {
+        return false;
+    }
+}
